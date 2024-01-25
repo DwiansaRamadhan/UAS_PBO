f1bb0e368600a230a36682b46174af7e1a6d16ae
Dwiansa <dwiansaramadhan1998@gmail.com>
2024-01-25 16:24:12 +0700

    update

---
+++ cr101/src/app/Filament/Resources/ProductsResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductsResource\Pages;
+use App\Filament\Resources\ProductsResource\RelationManagers;
+use App\Models\Products;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\FileUpload;
+
+class ProductsResource extends Resource
+{
+    protected static ?string $model = Products::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'Shop';
+
+    protected static ?int $navigationSort = 1;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('price')->required(),
+                Forms\Components\TextInput::make('slug'),
+                Forms\Components\FileUpload::make('image')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->sortable(),
+                Tables\Columns\TextColumn::make('price')->sortable(),
+                Tables\Columns\TextColumn::make('slug')->sortable(), 
+                Tables\Columns\ImageColumn::make('image')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProducts::route('/create'),
+            'edit' => Pages\EditProducts::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ cr101/src/app/Http/Controllers/ControllStatmentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ControllStatment;
+use Illuminate\Http\Request;
+
+class ControllStatmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ControllStatment  $controllStatment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ControllStatment $controllStatment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ControllStatment  $controllStatment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ControllStatment $controllStatment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ControllStatment  $controllStatment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ControllStatment $controllStatment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ControllStatment  $controllStatment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ControllStatment $controllStatment)
+    {
+        //
+    }
+}
